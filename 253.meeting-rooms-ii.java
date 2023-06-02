3e29a75e6f4b81114a11a5576ea0ba49b893d1de
Yuyi Wang <billyuyi@YuyideMac-mini.local>
2023-06-02 15:45:26 -0700

    today's leetcode

--- 253.meeting-rooms-ii.java
+++ 253.meeting-rooms-ii.java
@@ -5,10 +5,22 @@
  */
 
 // @lc code=start
+import java.util.*;
+
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        
+        List<int[]> points = new ArrayList<>();
+        for (int[] interval : intervals) {
+            points.add(new int[] { interval[0], 1 });
+            points.add(new int[] { interval[1], -1 });
+        }
+        Collections.sort(points, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        int cur = 0, ans = 0;
+        for (int[] point : points) {
+            cur += point[1];
+            ans = Math.max(ans, cur);
+        }
+        return ans;
     }
 }
 // @lc code=end
-
