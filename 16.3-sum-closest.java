57b3e3623a1c440fbcbf8760891a6303a2ccacfe
Yuyi Wang <billyuyi@YuyideMac-mini.local>
2023-06-05 17:55:05 -0700

    finished todays

--- 16.3-sum-closest.java
+++ 16.3-sum-closest.java
@@ -5,10 +5,30 @@
  */
 
 // @lc code=start
+import java.util.*;
+
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        
+        Arrays.sort(nums);
+        int diff = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if (sum == target) {
+                    return sum;
+                }
+                if (Math.abs(sum - target) < Math.abs(diff)) {
+                    diff = target - sum;
+                }
+                if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return target - diff;
     }
 }
 // @lc code=end
-
