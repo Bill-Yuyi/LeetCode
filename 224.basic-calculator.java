2552780e45f1a01633310ddd7bae495c65505041
Yuyi Wang <a274683718@gmail.com>
2023-06-13 17:03:46 -0700

    basic calculator

--- 224.basic-calculator.java
+++ 224.basic-calculator.java
@@ -10,28 +10,22 @@ import java.util.*;
 class Solution {
     public int calculate(String s) {
         Stack<Integer> st = new Stack<>();
-        int num = 0, res = 0, sign = 1;
-        char c = '1';
+        int num = 0, sign = 1, res = 0;
         for (int i = 0; i < s.length(); i++) {
-            c = s.charAt(i);
+            char c = s.charAt(i);
             if (Character.isDigit(c)) {
                 num = num * 10 + c - '0';
-            } else if (c == '+') {
-                res += sign * num;
-                sign = 1;
-                num = 0;
-            } else if (c == '-') {
-                res += sign * num;
-                sign = -1;
+            } else if (c == '+' || c == '-') {
+                res += num * sign;
+                sign = c == '+' ? 1 : -1;
                 num = 0;
             } else if (c == '(') {
                 st.push(res);
                 st.push(sign);
-
                 sign = 1;
                 res = 0;
             } else if (c == ')') {
-                res += sign * num;
+                res += num * sign;
                 res *= st.pop();
                 res += st.pop();
                 num = 0;
