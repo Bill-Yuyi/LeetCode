f75e9401139f71397eee36bfba80910131551ef2
Yuyi Wang <a274683718@gmail.com>
2023-07-09 20:42:30 -0700

    finished today

---
+++ 111.minimum-depth-of-binary-tree.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=111 lang=java
+ *
+ * [111] Minimum Depth of Binary Tree
+ */
+
+// @lc code=start
+
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        int res = 0;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (cur.left == null && cur.right == null) {
+                    return res;
+                }
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            res++;
+        }
+        return -1;
+
+    }
+}
+// @lc code=end
